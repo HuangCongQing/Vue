bf7e9712a01eab153d3529e21b74fece85cfa472
HuangChongqing <1756260160@qq.com>
2017-06-04 04:24:53 +0800

    router-view分享会代码（vue-cli搭建测试）

---
+++ vue-router/vue-router-project/src/App.vue
@@ -0,0 +1,77 @@
+<template>
+  <div id="app">
+    <p>
+      <!-- 使用 router-link 组件来导航. -->
+      <!-- 通过传入 `to` 属性指定链接. -->
+      <!-- <router-link> 默认会被渲染成一个 `<a>` 标签 -->
+      <router-link to="/Hu">找胡颖</router-link>
+      <router-link to="/Huang">找重庆</router-link>
+
+      <!-- 动态路由显示 -->
+      <p>动态路由显示
+         <router-link to="/user/去到25栋">有人：去到25栋</router-link>
+         <router-link to="/user/去到27栋">有人：去到27栋</router-link>
+      </p>
+      <!-- 嵌套路由 -->
+      <p>嵌套路由
+         <router-link to="/user1/中心">有人：中心</router-link>
+         <router-link to="/user1/千喜鹤">有人：千喜鹤</router-link>
+      </p>
+    </p>
+    <!-- 编程路由 -->编程路由
+    <div @click="goto()">点击：找人</div>
+    <div @click="goto1()">记录go():回退两次记录</div>
+    <!-- 命名路由 -->
+    <p>命名路由
+      <router-link :to="{ name: 'index', }">点击回到首页</router-link>
+    </p>
+    <!-- 重定向和命名 -->
+    <p>
+      <router-link to="重定向">重定向回重庆</router-link>
+    </p>
+    <!-- 导航钩子 -->
+    <p>导航钩子
+      <router-link to="导航钩子">导航钩子：指向牧牧</router-link>
+    </p>
+    <!-- 路由出口 -->
+   <!-- 路由匹配到的组件将渲染在这里 -->
+   <!-- 模板里引入router-view -->
+    <transition name="fade">
+    <router-view class="view one"></router-view>
+    </transition>
+    <!-- <router-view class="view two" name="a"></router-view>
+    <router-view class="view three" name="b"></router-view>
+    <router-view class="view four" name="c"></router-view> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  //
+  methods: {
+    goto: function () {
+      let url = '/lll/' + 1
+      // console.log(this.$router)
+      this.$router.push({
+        path: url
+      })
+    },
+    goto1: function () {
+      this.$router.go(-2)
+    }
+  }
+}
+
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-router/vue-router-project/src/router/index.js
@@ -0,0 +1,119 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+// 我直接写模板，就不引入组件了
+// 基础
+const Who = { template: '<transition name="slide"><div>首页：这是Who</div></transition>' }
+const Hu = { template: '<transition name="slide"><div>这是胡颖</div></transition>' }
+const Huang = { template: '<div>这是重庆</div>' }
+const Qi = { template: '<div>这是小青</div>' }
+// const Yuan = { template: '<div>这是圆哥</div>' }
+// const Huang = { template: '<div>这是重庆</div>' }
+const MU = { template: '<div>这是牧牧</div>' }
+const Tan = { template: '<div>这是唐燕</div>' }
+const Bai = { template: '<div>这是白帮议</div>' }
+const Yang = { template: '<div>这是杨怡</div>' }
+// 动态路由匹配模板
+const User = {
+  template: '<div>User {{ $route.params.id }}</div>'
+}
+// 嵌套路由模板
+const User1 = {
+  template: `
+    <div class="user">
+      <h2>User {{ $route.params.id }}</h2>
+      <router-view></router-view>
+    </div>
+  `
+}
+const people1 = {
+  template: '<div>牧牧去{{ $route.params.id }}</div>'
+}
+const people2 = {
+  template: '<div>people2 {{ $route.params.id }}</div>'
+}
+//
+// 如果使用模块化机制编程,要调用 Vue.use(Router)
+// .use(插件)，引入插件
+Vue.use(Router)
+
+export default new Router({
+  // history模式
+  mode: 'history',
+  routes: [
+  // 这里面的组件都通过<router-view></router-view>渲染出来
+    {
+      // 直接进入界面就能看到
+      path: '/',
+      name: 'index',
+      components: {
+        default: Who,
+        a: Tan,
+        b: Bai,
+        c: Yang
+      }
+    },
+    {
+      path: '/Hu',
+      name: 'Hu',
+      component: Hu
+    },
+    {
+      path: '/Huang',
+      name: 'Huang',
+      component: Huang
+    },
+    // 动态路径参数 以冒号开头
+    { path: '/user/:id',
+      name: 'user',
+      component: User,
+      beforeEnter: (to, from, next) => {
+        // next(false)
+      }
+    },
+    // 嵌套路由
+    { path: '/user1/:id',
+      component: User1,
+      children: [
+        {
+          // 当 /user1/:id/ 匹配成功，
+          // people1 会被渲染在 User1 的 <router-view> 中
+          path: '/',
+          component: people1
+        },
+        {
+          // 当 /user1/:id/people2 匹配成功
+          // people2 会被渲染在 User 的 <router-view> 中
+          path: '/people2',
+          component: people2
+        }
+      ]
+    },
+    // 编程路由
+    // path路径和goto函数里的路径相同
+    { path: '/lll/1',
+      component: Qi
+    },
+    // 重定向和命名
+    {
+      path: '/重定向',
+      redirect: '/Huang'
+    },
+    // 导航钩子
+    {
+      path: '/导航钩子',
+      component: MU,
+      beforeEnter: (to, from, next) => {
+        // next(false),
+        console.log(to)
+        console.log(from)
+        console.log(next)
+        next('/')// 指向首页
+      }
+    }
+  ],
+  // 滚动行为
+  scrollBehavior (to, from, savedPosition) {
+    // return 期望滚动到哪个的位置
+  }
+})
